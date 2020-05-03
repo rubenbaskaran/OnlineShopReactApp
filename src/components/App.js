44f964e373173f43fccbb22ee4c5f1e361f441fc
Ruben Baskaran <rbaskaran92@gmail.com>
2020-05-03 21:03:12 +0200

    added homepage

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import About from "./about";
+import Home from "./home";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route path="/about" component={About} />
+        <Route path="/home" component={Home} />
       </Switch>
     </div>
   );
