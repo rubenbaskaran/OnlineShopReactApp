b73eaaddf3acddc3c8b6e6259a9aed4718120ccc
Ruben Baskaran <rbaskaran92@gmail.com>
2020-05-05 12:17:06 +0200

    added additional fields for sign up form

--- src/components/RegisterLogin/register.js
+++ src/components/RegisterLogin/register.js
@@ -1,9 +1,146 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Register extends Component {
-  state = {};
+  state = {
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+    errors: [],
+  };
+
   render() {
-    return <div>Hello register page</div>;
+    return (
+      <div className="container">
+        <h2>Sign Up</h2>
+        <div className="row">
+          <form
+            className="col s12"
+            onSubmit={(event) => this.submitForm(EventSource)}
+          >
+            <div className="row">
+              <div className="input-field col s12">
+                <input
+                  name="firstname"
+                  value={this.state.firstname}
+                  onChange={(e) => this.handleChange(e)}
+                  id="firstname"
+                  type="text"
+                  className="validate"
+                />
+
+                <label htmlFor="email">Firstname</label>
+                <span
+                  className="helper-text"
+                  data-error="Type a right type email"
+                  data-success="right"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="input-field col s12">
+                <input
+                  name="lastname"
+                  value={this.state.lastname}
+                  onChange={(e) => this.handleChange(e)}
+                  id="lastname"
+                  type="text"
+                  className="validate"
+                />
+
+                <label htmlFor="email">Lastname</label>
+                <span
+                  className="helper-text"
+                  data-error="Type a right type email"
+                  data-success="right"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="input-field col s12">
+                <input
+                  name="email"
+                  value={this.state.email}
+                  onChange={(e) => this.handleChange(e)}
+                  id="email"
+                  type="email"
+                  className="validate"
+                />
+
+                <label htmlFor="email">Email</label>
+                <span
+                  className="helper-text"
+                  data-error="Type a right type email"
+                  data-success="right"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="input-field col s12">
+                <input
+                  name="password"
+                  value={this.state.password}
+                  onChange={(e) => this.handleChange(e)}
+                  id="password"
+                  type="password"
+                  className="validate"
+                />
+
+                <label htmlFor="password">Password</label>
+                <span
+                  className="helper-text"
+                  data-error="wrong"
+                  data-success="right"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="input-field col s12">
+                <input
+                  name="passwordConfirmation"
+                  value={this.state.passwordConfirmation}
+                  onChange={(e) => this.handleChange(e)}
+                  id="passwordConfirmation"
+                  type="password"
+                  className="validate"
+                />
+
+                <label htmlFor="password">Password Confirmation</label>
+                <span
+                  className="helper-text"
+                  data-error="wrong"
+                  data-success="right"
+                />
+              </div>
+            </div>
+
+            {this.state.errors.length > 0 && (
+              <div>{this.displayErrors(this.state.errors)}</div>
+            )}
+
+            <div className="row">
+              <div className="col 12">
+                <button
+                  className="btn waves-effect red lighten-2"
+                  type="submit"
+                  name="action"
+                  onClick={this.submitForm}
+                >
+                  Create an account
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+        Login Page
+      </div>
+    );
   }
 }
 
